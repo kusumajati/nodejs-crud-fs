01cf132c38c81f8c56e14d531999bffe9df059f4
kusumajati <nino.bmakj@outlook.com>
2019-09-12 01:56:47 +0700

    heroku init

--- README.md
+++
@@ -1,3 +0,0 @@
-# nodejs-express-fs
-
-CRUD cycle useing express and node file system
--- app.js
+++ app.js
@@ -1,18 +1,95 @@
-
-const app = require('express')()
+const express = require('express')
+const app = express()
 const PORT = 3001
 const uuid = require('uuid')
 var users = require('./user.json')
 const bodyParser = require('body-parser')
 const fs = require('fs')
+const user = require('./middleware/user')
+const log = require("./middleware/log")
+const multer = require('multer')
+const path = require('path')
+
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now()+path.extname(file.originalname)||".jpg")
+    }
+  })
+   
+  var upload = multer({ storage: storage })
+
+// var upload = multer({ dest: 'http://res.cloudinary.com/ninocloudinary' })
 
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
+app.use(express.static('public'))
+
+
+app.use('/static', express.static('public'))
+
+
+app.post('/upload/:id',  upload.single('avatar'), (req,res)=>{
+    var findIndex = users.findIndex(user => user.id === req.params.id)
+    if(findIndex>-1){
+        res.redirect('/patch-image/'+req.params.id+"/"+req.file.filename)
+    }else{
+
+    }
+})
+app.get('/patch-image/:id/:imageId', (req,res)=>{
+    var findIndex = users.findIndex(user => user.id === req.params.id)
+    if(findIndex>-1){
+        var pushed = {
+            id: req.params.id, name: req.body.name || users[findIndex].name,
+            email: req.body.email || users[findIndex].email, nohp: req.body.nohp || users[findIndex].nohp
+            ,image: req.params.imageId
+        }
+        users.splice(findIndex, 1, pushed)
+        fs.writeFile("user.json", JSON.stringify(users), (err) => {
+            if (err) {
+                res.json({
+                    success: false,
+                    message: "fail to create user",
+                    data: err
+                })
+            } else {
+                res.json({
+                    success: true,
+                    message: "new user created",
+                    data: pushed
+                })
+            }
+        })
+    }else{
+
+
+    }
+
+})
+app.get("/user/show/:id",(req,res)=>{
+    var user = users.find(user => user.id === req.params.id)
+    if (user) {
+        res.render("UserShow.ejs", {user:user})
+    } else {
+        res.json({
+            success: false,
+            message: "user not found",
+        })
+    }
+
+})
+
 app.get('/', function (req, res) {
     // console.log("Hello World")
-    res.send('Hello World')
+    
+    res.redirect('/user')
 })
+
 app.get('/write-file-sync', (req, res) => {
     fs.writeFile("text.js", "Hello World2", (err) => {
         if (err) {
@@ -22,7 +99,7 @@ app.get('/write-file-sync', (req, res) => {
         }
     })
 })
-app.get('/user', (req, res) => {
+app.get('/user', user,log,  (req, res) => {
     res.send(users)
 })
 app.get('/user/:id', (req, res) => {
@@ -40,8 +117,15 @@ app.get('/user/:id', (req, res) => {
         })
     }
 })
-app.post('/user', (req, res) => {
-    var pushed = { name: req.body.name, nohp: req.body.nohp, email: req.body.email, id: uuid() }
+app.post('/user', upload.single('avatar'),(req, res) => {
+    var pushed = { 
+        name: req.body.name,
+         nohp: req.body.nohp, 
+         email: req.body.email, 
+         id: uuid(), 
+         image:"/"+req.file.filename 
+        }
+    console.log(req.body)
     users = [...users, pushed]
     fs.writeFile("user.json", JSON.stringify(users), (err) => {
         if (err) {
